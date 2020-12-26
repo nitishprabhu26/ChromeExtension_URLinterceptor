eb292d711f307d895e65099b4aa88d6a5bfb8fe9
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-12-26 22:59:38 +0530

    Added files- first commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function(){
+    
+//     // chrome storage api
+//     // sync storage- syncs it across all browser tabs
+//     chrome.storage.sync.get(["focus"], function(result){
+//         // 1st time it wont get executed
+//         if(result.focus){
+//             focusInput.value = result.focus
+//         }
+//     });
+
+//     chrome.storage.sync.set({focus: e.target.value});
+
+// }, false);
+
