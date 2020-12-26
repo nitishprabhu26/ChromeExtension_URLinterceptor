c3e37de2c3b350bb8648eb3e56da2cfd407a6263
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2020-12-27 00:01:16 +0530

    Blocking a hardcoded list of URL's

--- script/script.js
+++ script/script.js
@@ -1,8 +1,20 @@
-// using webRequest, using the method onBeforeRequest
+// using the chrome webRequest API, using the method onBeforeRequest
 // for "all url's" and the type is "BLOCKING"
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(details) {
+//         return {cancel: true}
+//     }, { urls: ["<all_urls>"]}, ["blocking"]
+// )
+
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        return {cancel: true}
+        const urlsToBlock = ["https://twitter.com", "https://www.facebook.com"];
+        console.log(details);
+        if(urlsToBlock.includes(details.initiator)){
+            return {cancel: true};
+        }else{
+            return {cancel: false};
+        }
     }, { urls: ["<all_urls>"]}, ["blocking"]
 )
-
